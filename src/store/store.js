802d74f997022ab0096dc5c3f1159b18fb48141f
difo23 <diffozanzan@gmail.com>
2020-11-09 23:11:27 -0400

    Add redux tools

--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,6 @@ const reducers = combineReducers({
 })
 
 export const store = createStore(
-    reducers
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
